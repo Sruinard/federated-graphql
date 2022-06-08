c51e669fd7a321bf2c21cd444b2acc5f8e33ee96
Sruinard <41334908+Sruinard@users.noreply.github.com>
2022-06-08 11:46:19 +0000

    first setup.

---
+++ src/products/products/main.py
@@ -0,0 +1,23 @@
+import strawberry
+
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+import uvicorn
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
+
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9090)
