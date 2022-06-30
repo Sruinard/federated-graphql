5c4e9283e0caae681ed84436324ce1bde3da5811
Stef Ruinard <stefruinard@hotmail.com>
2022-06-30 15:33:55 +0200

    update cicd

--- deployment/apps/upload_graphql_schema.sh
+++ deployment/apps/upload_graphql_schema.sh
@@ -3,9 +3,9 @@
 RESOURCE_GROUP=$1
 ENDPOINT=$(az deployment group show -g ${RESOURCE_GROUP} -n main --query properties.outputs.graphqlEndpoint.value)
 echo $ENDPOINT | xargs get-graphql-schema > schema.graphql
+
 az apim api schema create --service-name $(az deployment group show -g ${RESOURCE_GROUP} -n main --query properties.outputs.apimName.value) \
     -g ${RESOURCE_GROUP} \
-    -n main \
     --api-id $(az deployment group show -g ${RESOURCE_GROUP} -n main --query properties.outputs.apiId.value) \
     --schema-id graphql \
     --schema-type application/vnd.ms-azure-apim.graphql.schema \
